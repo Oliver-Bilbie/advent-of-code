6b1e503e357fa47c4b0fdee28fe9f0407b7ee074
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2024-12-16 17:47:00 +0000

    2024 Day 16

---
+++ 2024/day_16/task_1/src/main.rs
@@ -0,0 +1,164 @@
+use aoc_utils::{
+    direction::*,
+    graph::graph::{Edge, Graph},
+    position::*,
+};
+use std::fs;
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Tile {
+    position: Position,
+    direction: Direction,
+}
+
+struct Maze {
+    graph: Graph<Tile>,
+    start: Position,
+    finish: Position,
+}
+
+fn read_maze(input_file: &str) -> Maze {
+    let mut graph = Graph::new();
+
+    let tiles: Vec<Vec<char>> = fs::read_to_string(input_file)
+        .unwrap()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let boundary = Position {
+        row: tiles.len(),
+        column: tiles.first().unwrap().len(),
+    };
+
+    // Create a graph where the nodes have the dimensions of (row, column, entry_direction)
+    // since nodes in the same physical position are reachable in different numbers of moves
+    // depending on which direction they are approached from.
+    for row in 0..boundary.row {
+        for column in 0..boundary.column {
+            for source_direction in [
+                Direction::Up,
+                Direction::Down,
+                Direction::Left,
+                Direction::Right,
+            ] {
+                let source = Tile {
+                    position: Position { row, column },
+                    direction: source_direction.clone(),
+                };
+
+                for travel_direction in [
+                    Direction::Up,
+                    Direction::Down,
+                    Direction::Left,
+                    Direction::Right,
+                ] {
+                    if let Some(destination_position) =
+                        travel_direction.travel_with_bounds(&Position { row, column }, &boundary)
+                    {
+                        if tiles[destination_position.row][destination_position.column] != '#' {
+                            let destination = Tile {
+                                position: destination_position,
+                                direction: travel_direction.clone(),
+                            };
+
+                            let weight = if travel_direction == source_direction {
+                                1 // Move forward
+                            } else {
+                                1001 // Turn and move
+                            };
+
+                            graph.add_edge(Edge {
+                                source: source.clone(),
+                                destination,
+                                weight,
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let start = tiles
+        .iter()
+        .enumerate()
+        .find_map(|(row, inner_vec)| {
+            inner_vec
+                .iter()
+                .position(|&x| x == 'S')
+                .map(|column| Position { row, column })
+        })
+        .unwrap();
+    let finish = tiles
+        .iter()
+        .enumerate()
+        .find_map(|(row, inner_vec)| {
+            inner_vec
+                .iter()
+                .position(|&x| x == 'E')
+                .map(|column| Position { row, column })
+        })
+        .unwrap();
+
+    Maze {
+        graph,
+        start,
+        finish,
+    }
+}
+
+fn find_minimum_distance(maze: &mut Maze) -> Option<u128> {
+    let start = Tile {
+        position: maze.start.clone(),
+        direction: Direction::Right,
+    };
+
+    maze.graph.dijkstra(start).unwrap();
+
+    // Attempt to approach the end tile from all directions and take the
+    // shortest distance
+    let mut min_distance: Option<u128> = None;
+    for direction in [
+        Direction::Up,
+        Direction::Down,
+        Direction::Left,
+        Direction::Right,
+    ] {
+        let finish = Tile {
+            position: maze.finish.clone(),
+            direction,
+        };
+        if let Some(distance) = maze.graph.get_node_distance(&finish) {
+            match min_distance {
+                Some(current_min) => {
+                    if distance < current_min {
+                        min_distance = Some(distance)
+                    }
+                }
+                None => min_distance = Some(distance),
+            }
+        }
+    }
+
+    min_distance
+}
+
+fn main() {
+    let mut maze = read_maze("../input.txt");
+    let min_distance = find_minimum_distance(&mut maze);
+    println!("The minimum distance is: {}", min_distance.unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_the_example() {
+        let mut maze = read_maze("../test_input.txt");
+        let actual_distance = find_minimum_distance(&mut maze);
+        let expected_distance = Some(7036);
+        assert_eq!(actual_distance, expected_distance);
+    }
+}
