7de94c2543c4e76ea98ca65575f6be005ce174de
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-04-15 13:57:14 +0100

    UI Styling

--- frontend/main.js
+++ frontend/main.js
@@ -8,12 +8,27 @@ async function start() {
     const day = parseInt(document.getElementById("day").value, 10);
     const input = document.getElementById("input").value;
 
+    document.getElementById("output-part-1").textContent = "Processing...";
+    document.getElementById("output-part-2").textContent = "Queued";
+    document.getElementById("output-container").style.visibility = "visible";
+
     try {
       const result = run(year, day, 1, input);
-      document.getElementById("output").textContent = result;
+      document.getElementById("output-part-1").textContent = result;
     } catch (e) {
       console.error(e);
-      document.getElementById("output").textContent =
+      document.getElementById("output-part-1").textContent =
+        `An error has occurred while running the solution; please check your input.\n${e}`;
+    }
+
+    document.getElementById("output-part-2").textContent = "Processing...";
+
+    try {
+      const result = run(year, day, 2, input);
+      document.getElementById("output-part-2").textContent = result;
+    } catch (e) {
+      console.error(e);
+      document.getElementById("output-part-2").textContent =
         `An error has occurred while running the solution; please check your input.\n${e}`;
     }
   });
