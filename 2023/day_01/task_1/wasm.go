cbfb6faeb75b382a470208c2bd06f7dfbcd617c4
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-11-30 22:55:02 -0600

    Go refactor

--- 2023/day_01/task_1/wasm.go
+++ 2023/day_01/task_1/wasm.go
@@ -1,22 +1,16 @@
+//go:build js
+
 package main
 
 import (
-	"unsafe"
+	"syscall/js"
 )
 
-// We store the result as a global variable to prevent it being
-// lost to GC before the caller can read it
-var result []byte
+func main() {
+	js.Global().Set("solve", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		input := args[0].String()
+		return Solve(input)
+	}))
 
-//export solve
-func solve(ptr uint32, size uint32) uint64 {
-	inputBytes := unsafe.Slice((*byte)(unsafe.Pointer(uintptr(ptr))), size)
-	input := string(inputBytes)
-
-	result = []byte(Solve(input))
-
-	resultPtr := uintptr(unsafe.Pointer(&result[0]))
-	resultLen := uint64(len(result))
-
-	return (uint64(resultPtr) << 32) | resultLen
+	select {}
 }
--- 2023/day_01/task_2/main.go
+++ 2023/day_01/task_2/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"unsafe"
 )
-
-// We store the result as a global variable to prevent it being
-// lost to GC before the caller can read it
-var result []byte
 
 func main() {
 	input, err := os.ReadFile("../input.txt")
@@ -17,17 +12,4 @@ func main() {
 		return
 	}
 	fmt.Println(Solve(string(input)))
-}
-
-//export solve
-func solve(ptr uint32, size uint32) uint64 {
-	inputBytes := unsafe.Slice((*byte)(unsafe.Pointer(uintptr(ptr))), size)
-	input := string(inputBytes)
-
-	result = []byte(Solve(input))
-
-	resultPtr := uintptr(unsafe.Pointer(&result[0]))
-	resultLen := uint64(len(result))
-
-	return (uint64(resultPtr) << 32) | resultLen
 }
