0c5e757547a237b9c5aee1794bcb47c86d9cde8f
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2026-01-01 16:54:03 -0600

    Added Python init support

---
+++ init_utils/templates/python/solution.py
@@ -0,0 +1,7 @@
+def solve(input_file: str) -> str:
+    return f"The result is: {result(input_file)}"
+
+
+def result(input_file: str) -> int:
+    print(f"Recieved input:\n{input_file}")
+    return 123
